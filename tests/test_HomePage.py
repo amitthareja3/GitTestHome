6794512900b3420d185187afc7c9605ad3adaab8
Amit Thareja <amitthareja3@yahoo.com>
2024-03-17 15:58:35 +0530

    MAde another change

--- tests/test_HomePage.py
+++ tests/test_HomePage.py
@@ -41,3 +41,21 @@ class TestHomePage(BaseClass):
     @pytest.fixture2(params=HomePageData.getTestData("Testcase2"))
     def getdata(self, request):
         return request.param
+
+    def test_formSubmission(self,getdata):
+        logs = self.getLogger()
+        homePage = HomePage(self.driver)
+
+        logs.info("First Name is" + getdata["firstname"])
+        homePage.getName().send_keys(getdata["firstname"])
+        homePage.getEmail().send_keys(getdata["lastname"])
+        homePage.getCheckBox().click()
+        self.SelectOptionByText(homePage.getGender(),getdata["gender"])
+        homePage.getSubmit().click()
+
+        alertText = homePage.getSuccessMessage().text
+
+        assert ("Success" in alertText)
+        alertText1 = homePage.getSuccessMessage().text
+
+        self.driver.refresh()
